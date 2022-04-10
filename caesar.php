3f038637ba6aaab70a74f97cc03ee4b4e113f79b
isobarbaric <shahkrish2016@gmail.com>
2022-04-10 00:56:25 -0400

    first commit

---
+++ caesar.php
@@ -0,0 +1,31 @@
+<?php
+
+    function wordExists($wordToCheck) {
+        $context = stream_context_create(array(
+            'http' => array('ignore_errors' => true),
+        ));
+        $result = file_get_contents('https://www.merriam-webster.com/dictionary/'.$wordToCheck, false, $context);
+        if (substr_count($result, 'class="spelling-suggestion-text"') > 0) 
+            return 0;
+        if (substr_count($result, 'class="secondary"') > 0) 
+            return 0;
+        return 1;
+    }
+
+    function performCaesarShift($stringToShift, $shift) {
+        $revised = "";
+        for ($i = 0; $i < strlen($stringToShift); $i++) {
+            $revised .= chr(((ord($stringToShift[$i]) + 7 + $shift) % 26) + 97);
+        }
+        return $revised;
+    }
+
+    function findWords($wordEntered) {
+        $decryptedWords = [];
+        for ($shift_amt = 0; $shift_amt < 26; $shift_amt++) 
+            if (wordExists(performCaesarShift($wordEntered, $shift_amt))) 
+                array_push($decryptedWords, performCaesarShift($wordEntered, $shift_amt));
+        return $decryptedWords;  
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Caesar Cipher Decryptor</title>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&family=Open+Sans&family=Source+Code+Pro&family=Space+Mono&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div id="input">
+            <form action="index.php" method="GET">
+                <h3>Enter the encrypted word:</h3> 
+                <input type="text" name="encrpyted-word" id="textfield"> 
+                <input type="submit" id="submit-btn">
+            </form> 
+        </div> 
+        <br>
+        <?php
+            include 'caesar.php';
+            $wordEntered = $_GET["encrpyted-word"];
+            if (!empty($wordEntered)) { 
+                $wordEntered = $_GET["encrpyted-word"];
+                $decryptedWords = findWords($wordEntered);
+                echo '<div id="decrypted">\n<ul>\n';
+                echo count($decryptedWords)." word(s) were found...\n";
+                for ($i = 0; $i < count($decryptedWords); $i++) 
+                    echo "\n\t\t<li>".$decryptedWords[$i]."</li>";
+                echo "\n\t</ul>\n";
+            }
+        ?>
+    </body>
+</html>
